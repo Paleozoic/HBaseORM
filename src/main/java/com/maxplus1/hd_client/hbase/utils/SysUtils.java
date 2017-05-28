c0f75da4e12b4d5c5c65cc7e0b042e36dc36528c
DESKTOP-KMR7RKV\qxloo <573793638@qq.com>
2017-05-29 02:23:53 +0800

    update

--- src/main/java/com/maxplus1/hd_client/hbase/utils/SysUtils.java
+++ src/main/java/com/maxplus1/hd_client/hbase/utils/SysUtils.java
@@ -5,6 +5,13 @@ package com.maxplus1.hd_client.hbase.utils;
  */
 public enum SysUtils {
     ;
+
+    /**
+     * https://stackoverflow.com/questions/8703678/how-can-i-check-if-a-class-belongs-to-java-jdk
+     * 判断clz是否是JDK的类
+     * @param clz
+     * @return
+     */
     public static boolean isJavaClass(Class<?> clz) {
         return clz != null && clz.getClassLoader() == null;
     }
